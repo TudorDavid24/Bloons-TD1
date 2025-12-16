00ef5bde507deea86fa7f354ed6e3eb4f00f2a6c
TudorDavid24 <tudor293david@gmail.com>
2025-12-16 10:51:11 +0100

    Ho aggiunto i file fondamentali per JSwing

---
+++ MyPanel.java
@@ -0,0 +1,26 @@
+import javax.swing.JPanel;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+class MyPanel extends JPanel {
+
+    public MyPanel() {
+        MyKeyAdapter KeyBoard = new MyKeyAdapter(this);
+        addKeyListener(KeyBoard);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(500,500);
+        
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+    }
+
+
+}
---
+++ SwingPaintDemo1.java
@@ -0,0 +1,29 @@
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+
+public class SwingPaintDemo1 {
+    
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+                
+            }
+        });
+
+    }
+    
+    public static void createAndShowGUI() {
+        
+        JFrame f = new JFrame("Bloons TD1");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        MyPanel p = new MyPanel();
+        f.add(p);
+        f.pack(); // Adatta la dimensione al contenuto (MyPanel.getPreferredSize)
+        f.setVisible(true);
+        
+        // Assicurati che il pannello (p) abbia il focus subito all'avvio
+        p.requestFocusInWindow(); 
+    }
+}
