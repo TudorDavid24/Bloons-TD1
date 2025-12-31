90ea2711a1ae27864a183b146789bd74551b863d
TudorDavid24 <tudor293david@gmail.com>
2025-12-31 16:09:22 +0100

    Aggiunta classe padre per tutte le strutture

---
+++ Structure.java
@@ -0,0 +1,24 @@
+
+public class Structure {
+    String title;
+    String cost;
+    String speed;
+    String description;
+    int raggioAzione;
+
+    public String getTitle() {
+        return title;
+    }
+    public String getCost() {
+        return cost;
+    }
+    public String getSpeed() {
+        return speed;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getRaggioAzione() {
+        return raggioAzione;
+    }
+}
