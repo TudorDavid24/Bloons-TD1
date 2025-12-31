6cc3007723b0044fb962741d294fd5b1df0e2b63
TudorDavid24 <tudor293david@gmail.com>
2025-12-31 16:10:24 +0100

    Aggiunto il codice necessario per verificare se l'utente ha cliccato un icona delle strutture del gioco

--- MyMouseAdapter.java
+++ MyMouseAdapter.java
@@ -6,6 +6,22 @@ public class MyMouseAdapter implements MouseListener {
     @Override
     public void mouseClicked(MouseEvent e) {
         System.out.println(e.getX() +" " +e.getY());
+
+        if (e.getX()>607 && e.getX()<607+33 && e.getY()>180&& e.getY()<180+33) {
+            System.out.println("Hai premuto la scimmia dart");
+        }
+        if (e.getX()>642 && e.getX()<642+33 && e.getY()>180&& e.getY()<180+33) {
+            System.out.println("Hai premuto la tack");
+        }
+        if (e.getX()>677 && e.getX()<677+33 && e.getY()>180&& e.getY()<180+33) {
+            System.out.println("Hai premuto la ice");
+        }
+        if (e.getX()>712 && e.getX()<712+33 && e.getY()>180&& e.getY()<180+33) {
+            System.out.println("Hai premuto la bomb");
+        }
+        if (e.getX()>747 && e.getX()<747+33 && e.getY()>180&& e.getY()<180+33) {
+            System.out.println("Hai premuto la scimmia super");
+        }
         // TODO Auto-generated method stub
         //throw new UnsupportedOperationException("Unimplemented method 'mouseClicked'");
     }
@@ -24,6 +40,7 @@ public class MyMouseAdapter implements MouseListener {
 
     @Override
     public void mouseEntered(MouseEvent e) {
+        
         // TODO Auto-generated method stub
         //throw new UnsupportedOperationException("Unimplemented method 'mouseEntered'");
     }
