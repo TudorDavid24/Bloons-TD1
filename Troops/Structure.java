0184b488ce6b819b8cd11c92c10d030363032b7c
TudorDavid24 <tudor293david@gmail.com>
2026-01-19 22:51:07 +0100

    Aggiunta delle coordinate X e Y per le truppe

---
+++ Troops/Structure.java
@@ -0,0 +1,38 @@
+package Troops;
+
+public class Structure {
+    String title;
+    String cost;
+    String speed;
+    String description;
+    int raggioAzione;
+    int x,y;
+
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getCost() {
+        return cost;
+    }
+    public String getSpeed() {
+        return speed;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getRaggioAzione() {
+        return raggioAzione;
+    }
+}
