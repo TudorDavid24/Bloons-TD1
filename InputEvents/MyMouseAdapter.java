328aad01b5b7a16f4cd8c3a5258ec682ad5e89e9
TudorDavid24 <tudor293david@gmail.com>
2026-01-19 22:53:52 +0100

    Aggiunto codice che permette di piazzare una truppa nel campo

---
+++ InputEvents/MyMouseAdapter.java
@@ -0,0 +1,88 @@
+package InputEvents;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import Troops.BombTower;
+import Troops.DartMonkey;
+import Troops.IceTower;
+import Troops.SuperMonkey;
+import Troops.Tack;
+import UI.MyPanel;
+
+public class MyMouseAdapter implements MouseListener {
+
+    DartMonkey D1 = new DartMonkey();
+    Tack T1 = new Tack();
+    IceTower I1 = new IceTower();
+    BombTower B1 = new BombTower();   
+    SuperMonkey S1 = new SuperMonkey();
+
+    MyPanel pannelloSuCuiLavorare;
+      public MyMouseAdapter(MyPanel p){
+        this.pannelloSuCuiLavorare = p;
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println(e.getX() +" " +e.getY());
+
+        if (e.getX()>607 && e.getX()<607+33 && e.getY()>180&& e.getY()<180+33) {
+            System.out.println("Hai premuto la scimmia dart");
+            pannelloSuCuiLavorare.immagineMouse = D1.dartMonkeyImage;
+        }
+        if (e.getX()>642 && e.getX()<642+33 && e.getY()>180&& e.getY()<180+33) {
+            System.out.println("Hai premuto la tack");
+            pannelloSuCuiLavorare.immagineMouse = T1.tackImage;
+        }
+        if (e.getX()>677 && e.getX()<677+33 && e.getY()>180&& e.getY()<180+33) {
+            System.out.println("Hai premuto la ice");
+            pannelloSuCuiLavorare.immagineMouse = I1.iceTowerImage;
+        }
+        if (e.getX()>712 && e.getX()<712+33 && e.getY()>180&& e.getY()<180+33) {
+            System.out.println("Hai premuto la bomb");
+            pannelloSuCuiLavorare.immagineMouse = B1.bombMonkeyImage;
+        }
+        if (e.getX()>747 && e.getX()<747+33 && e.getY()>180&& e.getY()<180+33) {
+            System.out.println("Hai premuto la scimmia super");
+            pannelloSuCuiLavorare.immagineMouse = S1.superMonkeyImage;
+        }
+
+        if (e.getX()>0 && e.getX()<600 && e.getY()>0&& e.getY()<600) {
+            System.out.println("Hai premuto nel campo");
+            pannelloSuCuiLavorare.mouseX = e.getX();
+            pannelloSuCuiLavorare.mouseY = e.getY();
+            pannelloSuCuiLavorare.StampaImmagine = true;
+            pannelloSuCuiLavorare.immagineMouse = null;
+            pannelloSuCuiLavorare.StampaImmagine = false;
+        }
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'mouseClicked'");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'mouseReleased'");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'mouseEntered'");
+    }
+    
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'mouseExited'");
+    }
+    
+}
