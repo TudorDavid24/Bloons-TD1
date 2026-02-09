e9f27a7166df7a5c54eec1c70bb748e3bafa88b1
TUDOR-PC\tudor <tudor293david@gmail.com>
2026-02-09 23:18:02 +0100

    Passaggio da Array statici ad Array dinamini, aggiunta di variabili e labels che indicano le statistiche del giocatore, cambio di come vengono disegnate le strutture e i proteili affinché possano ruotare, aggiunti 10 round di gioco totali, modifica di come vengono gestiti i palloncini per magior efficacia e aggiunta della funzione PlaySound() per far partire le esplosioni dei bloons

--- UI/MyPanel.java
+++ UI/MyPanel.java
@@ -1,21 +1,20 @@
+//#region Import
 package UI;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 
 import Bloons.Bloons;
-import InputEvents.MyKeyAdapter;
 import InputEvents.MyMouseAdapter;
 import InputEvents.MyMouseMotionAdapter;
+import Proiettili.Cannon;
 import Proiettili.Darts;
-import Troops.BombTower;
-import Troops.DartMonkey;
-import Troops.IceTower;
+import Proiettili.TacksBullets;
+import Sound.SoundManager;
 import Troops.Structure;
-import Troops.SuperMonkey;
-import Troops.Tack;
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -28,6 +27,7 @@ import java.awt.geom.AffineTransform;
 import java.io.File;
 
 import java.util.concurrent.CopyOnWriteArrayList;
+//#endregion
 
 public class MyPanel extends JPanel {
 
@@ -37,41 +37,33 @@ public class MyPanel extends JPanel {
         setupLabels();
         inizializzaAdapter();
 
+        soundManager.loadSound("PopBloon.wav", "PopBloon.wav");
+        soundManager.loadSound("CannonExplosion.wav", "CannonExplosion.wav");
+
         pannelloStatistiche.setLayout(null);
         pannelloStatistiche.setBackground(new Color(190, 218, 201)); 
         pannelloStatistiche.setBounds(615, 230, 157, 300);
         pannelloStatistiche.setVisible(false);
         add(pannelloStatistiche);
-        
-        D1A = new Structure[10];
-        //Iniziallizzazione dell'array Structure a null
-        for (int i = 0; i < D1A.length; i++) {
-            D1A[i] = null;
-        }
-
-        B1A = new CopyOnWriteArrayList<>();
-        ArrayDardi = new CopyOnWriteArrayList<>();
 
     }
 
     //Array delle strutture
-    public Structure D1A[];
-    public int NelD1A = 0;
+    public CopyOnWriteArrayList<Structure> structureArray = new CopyOnWriteArrayList<>();
 
     //Array dei palloncini
-    public CopyOnWriteArrayList<Bloons> B1A;
+    public CopyOnWriteArrayList<Bloons> bloonsArray = new CopyOnWriteArrayList<>();
 
     //Array dei dardi
-    public CopyOnWriteArrayList<Darts> ArrayDardi;
+    public CopyOnWriteArrayList<Darts> dartsArray = new CopyOnWriteArrayList<>();
 
-    //Truppe
-    DartMonkey D1 = new DartMonkey(this);
-    Tack T1 = new Tack(this);
-    IceTower I1 = new IceTower(this);
-    BombTower B1 = new BombTower(this);   
-    SuperMonkey S1 = new SuperMonkey(this);
+    //Array dei tack
+    public CopyOnWriteArrayList<TacksBullets> tackArray = new CopyOnWriteArrayList<>();
 
-    //Immagini
+    //Array delle palle da cannone
+    public CopyOnWriteArrayList<Cannon> cannonBallsArray = new CopyOnWriteArrayList<>();
+
+    //Immagini delle icone
     Image Bg = new ImageIcon("Immagini/BTD1_bg.png").getImage();
     Image dartImage = new ImageIcon("Immagini/BTD1_dart_button.png").getImage();
     Image tackImage = new ImageIcon("Immagini/BTD1_tack_button.png").getImage();
@@ -79,91 +71,114 @@ public class MyPanel extends JPanel {
     Image bombImage = new ImageIcon("Immagini/BTD1_bomb_button.png").getImage();
     Image superImage = new ImageIcon("Immagini/BTD1_super_button.png").getImage();
 
+    //Pannello informazioni
     public JPanel pannelloStatistiche = new JPanel();
 
     //Labels
-    public JLabel roundLabel, moneyLabel, livesLabel, towersLabel, startRoundLabel;
+    public JLabel roundLabelText, moneyLabelText, livesLabelText, towersLabel, startRoundLabel;
+    public JLabel roundLabelValue, moneyLabelValue, livesLabelValue;
     public Rectangle StartRectButton = new Rectangle(615, 535, 157, 45);
 
     //Labels Info scimmie
     public JLabel title, cost, speed;
     public JTextArea description;
 
+    //Valori di gioco
+    public int money = 650;
+    public int tempMoney = 0;
+    public int lives = 40;
+    public int numeroRound = 0;
+
     //Posizione del mouse
     public int mouseX, mouseY;
     public Image immagineMouse = null;
 
-    //Stampa immagine
-    public Boolean StampaImmagine = false;
+    //Stato del round
+    public boolean isRoundRunning = false;
 
-    //Logica Round
-    public int NumeroRound = 0;
-    public boolean RoundIsStarted = false;
-    int NumeroPalloncini=0;
+    //Gestore dei suoni
+    public SoundManager soundManager = new SoundManager();
 
-    boolean tuttiFiniti = false;
-
+    //Immagini dei proiettili
     Image dardoImg = new ImageIcon("Immagini/BTD1_dart.png").getImage();
+    Image tackImg = new ImageIcon("Immagini/BTD1_tack.png").getImage();
+    Image cannonImg = new ImageIcon("Immagini/BTD1_bomb.png").getImage();
     
     public void paintComponent(Graphics g) {
+
         Graphics2D g2d = (Graphics2D) g;
         super.paintComponent(g); // Pulisce lo schermo
 
-        // #region Disegno GUI di base
-        // 1. Disegna lo sfondo
+        //#region Disegno GUI di base
+        //Disegna lo sfondo
         g.drawImage(Bg, 0, 0, getWidth(), getHeight(), this);
 
-        // 2. Disegna il rettangolo del menu
+        //Disegna il rettangolo del menu
         g.setColor(new Color(191, 191, 191, 200));
         g.fillRect(605, 10, 177, 580);
 
-        // 3. Disegna le icone manuali
+        //Disegna le icone
         g.drawImage(dartImage, 607, 180, 33, 33, this);
         g.drawImage(tackImage, 642, 180, 33, 33, this);
         g.drawImage(iceImage, 677, 180, 33, 33, this);
         g.drawImage(bombImage, 712, 180, 33, 33, this);
         g.drawImage(superImage, 747, 180, 33, 33, this);
 
-        //4. Disegna il pulsante di start
+        //Disegna il pulsante di start
         g.setColor(new Color(78, 203, 75, 255));
         g.fillRect(StartRectButton.x, StartRectButton.y, StartRectButton.width, StartRectButton.height);
         //#endregion
 
+        //Disegna l'immagine del personaggio in base alle coordinate del mouse
         if (immagineMouse != null) {
             g.drawImage(immagineMouse, mouseX - 23, mouseY - 23, this);
         }
 
-        for (int i = 0; i < NelD1A; i++) {
-            Structure s = D1A[i];
-            AffineTransform vecchioDato = g2d.getTransform();
-            g2d.translate(s.getX() + 23, s.getY() + 23); 
-            g2d.rotate(s.angolo+90);
-            g2d.drawImage(s.StructureImage, -23, -23, this);
-            g2d.setTransform(vecchioDato);
+        //Disegno di tutto l'array delle strutture
+        for (int i = 0; i < structureArray.size(); i++) {
+                Structure s = structureArray.get(i);
+                AffineTransform vecchioDato = g2d.getTransform();
+                g2d.translate(s.getX() + 23, s.getY() + 23); 
+                g2d.rotate(s.angolo);
+                g2d.drawImage(s.StructureImage, -23, -23, this);
+                g2d.setTransform(vecchioDato);
         }
 
-        if (RoundIsStarted) {
-
-            for (Bloons b : B1A) {
-                g.drawImage(b.ImmagineBloons, b.getX(), b.getY(), 30, 42, this);
-            }
-        
-            for (Darts d : ArrayDardi) {
+        //Disegno di tutto l'array di palloncini
+        for (Bloons b : bloonsArray) {
+            g.drawImage(b.ImmagineBloons, b.getX(), b.getY(), 30, 42, this);
+        }
+    
+        //Disegno di tutto l'array di proiettili
+        for (Darts d : dartsArray) {
             AffineTransform vecchioDato = g2d.getTransform();
             g2d.translate(d.getX(), d.getY());
             g2d.rotate(Math.atan2(d.velY, d.velX));
-            g2d.drawImage(dardoImg, 0, 0, 30, 10, this);
+            g2d.drawImage(dardoImg, -7, -7, 30, 10, this);
             g2d.setTransform(vecchioDato);
-            }
-
         }
-}
+
+        //Disegno di tutto l'array di tack
+        for (TacksBullets t : tackArray) {
+            AffineTransform vecchioDato = g2d.getTransform();
+            g2d.translate(t.getX(), t.getY());
+            g2d.rotate(Math.atan2(t.velY, t.velX));
+            g.drawImage(tackImg, -10, -15, 12, 10, this);
+            g2d.setTransform(vecchioDato);
+        }
+
+        //Disegno di tutto l'array di cannoni
+        for (Cannon c : cannonBallsArray) {
+            g2d.drawImage(cannonImg, c.getX(), c.getY(), 15, 18, this);
+        }
+        //Controllo se il round è finito
+        fineRound();
+    }
 
     @Override
     public Dimension getPreferredSize() {
         return new Dimension(785,600);
     }
-
     public void setupLabels(){
 
         Color textColor = new Color(238, 255, 243);
@@ -172,11 +187,15 @@ public class MyPanel extends JPanel {
         try {
             Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("LuckiestGuy-Regular.ttf")).deriveFont((float)labelY);
 
-            roundLabel = createLabel("Round:", 620, 40, customFont, textColor);
-            moneyLabel = createLabel("Money:", 620, 70, customFont, textColor);
-            livesLabel = createLabel("Lives:", 620, 100, customFont, textColor);
+            roundLabelText = createLabel("Round:", 620, 40, customFont, textColor);
+            moneyLabelText = createLabel("Money:", 620, 70, customFont, textColor);
+            livesLabelText = createLabel("Lives:", 620, 100, customFont, textColor);
             towersLabel = createLabel("Build Towers", 620, 140, customFont, textColor);
             startRoundLabel = createLabel("Start Round", 630, 545, customFont, textColor);
+
+            roundLabelValue = createLabel(String.valueOf(numeroRound), 720, 40, customFont, textColor);
+            moneyLabelValue = createLabel(String.valueOf(money), 720, 70, customFont, textColor);
+            livesLabelValue = createLabel(String.valueOf(lives), 720, 100, customFont, textColor);
             
             title = new JLabel();
             cost = new JLabel();
@@ -216,63 +235,91 @@ public class MyPanel extends JPanel {
         Color textColor = new Color(24, 129, 25);
         if (pannelloStatistiche.isVisible()==false) {
             title = createMenuLabel(S1.getTitle(), 10, 5, textColor, pannelloStatistiche);
-            cost = createMenuLabel("Cost: " +S1.getCost(), 5, 50, textColor, pannelloStatistiche);
+            cost = createMenuLabel("Cost: " + S1.getCost(), 5, 50, textColor, pannelloStatistiche);
             speed = createMenuLabel("Speed: " + S1.getSpeed(), 5, 80, textColor, pannelloStatistiche);
             description = createMenuTextArea(S1.getDescription(), 5, 130, textColor, pannelloStatistiche);
         }
     }
     private void inizializzaAdapter(){
-        MyKeyAdapter KeyBoard = new MyKeyAdapter(this);
-        addKeyListener(KeyBoard);
         MyMouseMotionAdapter MouseMotionAdapter = new MyMouseMotionAdapter(this);
         addMouseMotionListener(MouseMotionAdapter);
         MyMouseAdapter MouseAdapter = new MyMouseAdapter(this);
         addMouseListener(MouseAdapter);
     }
 
-    public void GestisciRound(){
+    public void gestioneRound(){
 
-                switch (NumeroRound) {
+                switch (numeroRound) {
                     case 1:
-                        StartaNumeroThread(10);
+                        spawnPalloncini(10, 10);
+                        tempMoney = 100;
                     break;
                     case 2:
-                        StartaNumeroThread(20);
+                        spawnPalloncini(20, 10);
+                        tempMoney = 200;
                     break;
                     case 3:
-                        StartaNumeroThread(5);
+                        spawnPalloncini(30, 8);
+                        tempMoney = 250;
+                    break;
+                    case 4:
+                        spawnPalloncini(40, 7);
+                        tempMoney = 200;
+                    break;
+                        case 5:
+                        spawnPalloncini(50, 6);
+                        tempMoney = 250;
+                    break;
+                        case 6:
+                        spawnPalloncini(60, 5);
+                        tempMoney = 350;
+                    break;
+                        case 7:
+                        spawnPalloncini(70, 5);
+                        tempMoney = 300;
+                    break;
+                        case 8:
+                        spawnPalloncini(80, 4);
+                        tempMoney = 250;
+                    break;
+                        case 9:
+                        spawnPalloncini(90, 3);
+                        tempMoney = 100;
+                    break;
+                       case 10:
+                        spawnPalloncini(100, 2);
                     break;
             }
         }
 
-    private void StartaNumeroThread(int numeroPalloncini){
+    private void spawnPalloncini(int numeroPalloncini, int velBloons) {
 
-        B1A.clear(); // Pulisci la lista per il nuovo round
+        // Pulisci la lista per il nuovo round
+        bloonsArray.clear(); 
          
         for (int i = 0; i < numeroPalloncini; i++) {
             Bloons b = new Bloons(this);
-            b.setTempoAttesaIni(200 * i);
-            B1A.add(b); // Aggiungi alla lista
-            
-            new Thread(() -> {
-                b.run();
-                // Quando il palloncino finisce il percorso o scoppia:
-                B1A.remove(b); 
-                if (B1A.isEmpty()) {
-                    fineRound();
-                }
-            }).start();
+            b.setTempoAttesaIni((int)((200 * Math.random()) + 200 * i));
+            b.TempoAttesa = velBloons;
+            bloonsArray.add(b);
+            b.start();
         }
-    }
 
+    }
     private void fineRound() {
-        javax.swing.SwingUtilities.invokeLater(() -> {
+        if (bloonsArray.isEmpty() && isRoundRunning) {
             System.out.println("Round Finito!");
-            RoundIsStarted = false;
+            SwingUtilities.invokeLater(() -> {
             StartRectButton.setLocation(615, 535);
             startRoundLabel.setVisible(true);
+            isRoundRunning=false;
+            money += tempMoney;
+            moneyLabelValue.setText(String.valueOf(money));
+            });
             repaint();
-        });
+        }
     }
-
+    public void PlaySound(String soundName) {
+        soundManager.playSound(soundName);
+    }
 }
